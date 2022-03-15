4db6953aed4171017c21cbacb2e7ac95b653a0d2
BlurryFox47 <peterkuce.rar@gmail.com>
2022-03-15 23:41:19 +0100

    New Branch Commit

--- src/main/java/game/duckhunt/Assignment1.java
+++ src/main/java/game/duckhunt/Assignment1.java
@@ -4,5 +4,6 @@ public class Assignment1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         //I have no idea if this works
+        //Maybe it does not
     }
 }
