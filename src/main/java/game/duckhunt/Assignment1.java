ab9d070613934008a1a1e5a400fb52fe61619760
BlurryFox47 <peterkuce.rar@gmail.com>
2022-03-15 23:40:08 +0100

    Hello World Commit

--- src/main/java/game/duckhunt/Assignment1.java
+++ src/main/java/game/duckhunt/Assignment1.java
@@ -3,5 +3,6 @@ package game.duckhunt;
 public class Assignment1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        //I have no idea if this works
     }
 }
